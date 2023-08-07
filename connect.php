8919cfa70fd149a802fe090f482e170f70443f15
Piotr Tajanowicz <piotr.tajanowicz@kyndryl.com>
2023-08-07 16:48:05 +0200

    changes from laptop last

--- connect.php
+++ connect.php
@@ -71,6 +71,7 @@ if( isset($_ENV['ssldsn']) )
     }
     else
     {
+        
         error_log(__FILE__ . __LINE__ . " Connect to DB2 Failed");
         // error_log(__FILE__ . __LINE__ . $conn_string);
         // error_log(__FILE__ . __LINE__ . db2_conn_errormsg());
