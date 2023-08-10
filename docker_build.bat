6881c65a501952163b1d16d9246c8559ea2ec88a
Piotr Tajanowicz <piotr.tajanowicz@kyndryl.com>
2023-08-10 13:22:13 +0200

    update today 6

--- docker_build.bat
+++ docker_build.bat
@@ -1,2 +1,2 @@
-docker build -t kpes . --no-cache 
+docker build -t kpes --no-cache  --progress=plain  . 2> build.log
 docker run -dit -p 8090:8080  --name kpes -v C:/CETAapps/kPES:/var/www/html --env-file C:/CETAapps/kPES/dev_env.list kpes
