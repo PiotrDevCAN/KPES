9ca15c744cee1ffb49f0bd4a9a1c2faa7ead2101
Piotr Tajanowicz <piotr.tajanowicz@kyndryl.com>
2023-08-10 12:40:19 +0200

    update today 6

--- patch1.sh
+++ patch1.sh
@@ -3,20 +3,21 @@ sed -i 's:#DocumentRoot "/opt/app-root/src":DocumentRoot "/var/www/html/":g' /et
 #sed -i '$ a ServerName localhost' /etc/httpd/conf/httpd.conf
 #dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y
 #dnf update -y && dnf install -y php-pear php-devel
+mkdir -p /opt/ibm
 sed -i 's:variables_order = "GPCS":variables_order = "GPCES":g' /etc/php.ini
 
 rm -rf /var/cache/dnf && dnf remove -y nodejs && dnf clean all && dnf update -y && dnf upgrade -y
-# dnf install -y php-pear php-devel php-pdo unzip zip
-
-yum -y install sudo
-
-sudo yum install -y unixODBC-devel
-
-
-
-yum -y install php-pear php-devel php-pdo unzip zip
-## sudo yum -y install unixODBC-devel
-
-
+# dnf install unzip zip libpng-devel libjpeg-turbo-devel postgresql-devel
+# dnf install -y unzip zip 
+dnf install -y php-pear php-devel unzip zip 
 curl -sS https://getcomposer.org/installer | tac | tac | php -- --install-dir=/usr/local/bin --filename=composer
 chmod 777 /usr/local/bin/composer
+
+#echo "<?php phpinfo();?>" > /var/www/html/index.php
+#echo 'auto_prepend_file="/var/www/html/php/siteheader.php"' > /etc/php.d/rob.ini
+#echo 'auto_append_file="/var/www/html/php/sitefooter.php"' >> /etc/php.d/rob.ini
+#echo 'memory_limit=512M' >> /etc/php.d/rob.ini
+
+# 
+# docker run -dit -p 80:8080 -v /root/local/sbe-dashboard:/var/www/html/ --name sbe sbe
+# docker run -dit -p 80:8080 --name rob rob
