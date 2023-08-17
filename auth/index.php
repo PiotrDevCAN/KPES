53689b377e676fd09a805cb0ef48442c2956dcf1
Piotr Tajanowicz <piotr.tajanowicz@kyndryl.com>
2023-08-17 14:28:12 +0200

    update day 10 SSO

--- auth/index.php
+++ auth/index.php
@@ -5,10 +5,11 @@ include realpath(dirname(__FILE__))."/../class/include.php";
 $auth = new Auth();
 if($auth->verifyResponse($_GET))
 {
-    // header("Access-Control-Allow-Origin: *");
-    // header("Location: ".$_GET['state']);
     $landingPage = urldecode($_GET['state']);
     echo 'Redirect to the final page '.$landingPage;
     echo 'Logged in successfully';
+    sleep(20);
+    header("Access-Control-Allow-Origin: *");
+    header("Location: ".$landingPage);
 	exit();
 }
