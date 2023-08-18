c96a61b1201d3dc3fc9509f97aa6ed27c8798ded
Piotr Tajanowicz <piotr.tajanowicz@kyndryl.com>
2023-08-18 11:54:28 +0200

    update day NEXT

--- batchJobs/testEmail.php
+++ batchJobs/testEmail.php
@@ -1,8 +1,25 @@
 <?php
-use itdq\BlueMail;
 
-$to = array();
-$to[] = 'piotr.tajanowicz@ocean.ibm.com';
+use itdq\Process;
 
-$replyto = $_ENV['noreplyemailid'];
-BlueMail::send_mail($to, 'Test', '<h1>Testing 1 2 3</h1>', $replyto);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$scriptsDirectory = '/var/www/html/batchJobs/';
+$processDirectory = 'processesCLI/'; 
+$processFile = 'testEmail.php';
+try {
+    $cmd = 'php ';
+    $cmd .= '-d auto_prepend_file=' . $scriptsDirectory . 'php/siteheader.php ';
+    $cmd .= '-d auto_append_file=' . $scriptsDirectory . 'php/sitefooter.php ';
+    $cmd .= '-f ' . $scriptsDirectory . $processDirectory. $processFile;
+    $process = new Process($cmd);
+    $pid = $process->getPid();
+    echo "The kPES Starters Send Out Script has succeed to be executed: " . $scriptsDirectory . $processDirectory . $processFile;
+    error_log("The kPES Starters Send Out Script has succeed to be executed: " . $scriptsDirectory . $processDirectory . $processFile);
+} catch (Exception $exception) {
+    echo $exception->getMessage();
+    echo "The kPES Starters Send Out Script has failed to be executed: " . $scriptsDirectory . $processDirectory . $processFile;
+    error_log("The kPES Starters Send Out Script has failed to be executed: " . $scriptsDirectory . $processDirectory . $processFile);
+}
