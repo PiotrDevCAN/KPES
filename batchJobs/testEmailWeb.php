26c6e2aa8e5c02be2f5d607dd2725eef4b35b914
Piotr Tajanowicz <piotr.tajanowicz@kyndryl.com>
2023-08-18 15:19:13 +0200

    update day SMPT

--- batchJobs/testEmailWeb.php
+++ batchJobs/testEmailWeb.php
@@ -5,5 +5,5 @@ $to = array();
 $to[] = 'piotr.tajanowicz@ocean.ibm.com';
 
 $replyto = $_ENV['noreplyemailid'];
-BlueMail::send_mail($to, 'Test', '<h1>Testing 1 2 3 as BatchJob</h1>', $replyto);
+BlueMail::send_mail($to, 'Test', '<h1>Testing 1 2 3</h1>', $replyto);
 
